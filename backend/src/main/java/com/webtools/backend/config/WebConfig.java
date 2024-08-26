9075156d5cd1500f5af1170c54f10eb5accd9379
Brandon Cheng <chen7381@umn.edu>
2024-08-26 11:18:39 +0800

    modified local java build version, added webconfig for local host api mapping

---
+++ backend/src/main/java/com/webtools/backend/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.webtools.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// for hosting frontend and backend locally, map APIs
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
